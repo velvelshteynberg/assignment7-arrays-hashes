5338f2ee8e0b9ac556b2f72cf15fb206cac19c85
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-20 22:15:32 -0500

    first practice

---
+++ excercise2.rb
@@ -0,0 +1,49 @@
+def output(name)
+    puts "#{name}"
+end
+
+grocery_list = ["*carrots", "*toilet paper", "*apples", "*salmon"]
+
+# what does the asterik mean? 
+
+puts output(grocery_list)
+
+grocery_list.push("*rice")
+
+puts output(grocery_list)
+
+puts grocery_list.length
+
+
+# grocery_list.each do |grocery_item|
+#     if grocery_item == "banana"
+#         puts "You need to pick up bananas"
+#     else
+#         puts "You don't need to pick up bananas today"
+#     end
+# end
+
+puts grocery_list.include?("banana")
+
+#how do you make it that it doesnt display that line 5 times
+
+
+output(grocery_list[1])
+
+
+puts grocery_list.sort
+
+
+grocery_list.delete("*salmon")
+
+puts grocery_list
+
+
+
+
+
+
+
+
+
+
